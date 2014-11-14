69a879fcdb6eddbe191c15dad71c0404f1b05bbe
martinow <martinow@sec.gov>
2014-11-14 15:17:08 -0500

    fixed a typo & imported the base data
    Signed-off-by: martinow <martinow@sec.gov>

--- octopus/case/forms.py
+++ octopus/case/forms.py
@@ -2,33 +2,33 @@ from flask_wtf import Form
 from wtforms import StringField, SelectField
 from wtforms.validators import DataRequired, Optional
 from wtforms.fields.html5 import DateField, IntegerField
-from .models import Region
+# from .models import Region
 
-class CaseForm(Form):
-    crd_number = IntegerField('CRD Number',
-                    validators=[DataRequired()])
-    start_date = DateField('Start Date',
-                             validators=[DataRequired()])
-    end_date = DateField('End Date', validators=[Optional()])
-    case_type = StringField('Case Type',
-                                validators=[DataRequired()])
-    case_region = SelectField("Regional Office", validators=[DataRequired()], choices=Regions)
-
-    def __init__(self, *args, **kwargs):
-        super(CaseForm, self).__init__(*args, **kwargs)
-        self.user = None
-
-    def validate(self):
-        initial_validation = super(CaseForm, self).validate()
-        if not initial_validation:
-            return False
-        user = User.query.filter_by(username=self.username.data).first()
-        if user:
-            self.username.errors.append("Username already registered")
-            return False
-        user = User.query.filter_by(email=self.email.data).first()
-        if user:
-            self.email.errors.append("Email already registered")
-            return False
-        return True
+# class CaseForm(Form):
+#     crd_number = IntegerField('CRD Number',
+#                     validators=[DataRequired()])
+#     start_date = DateField('Start Date',
+#                              validators=[DataRequired()])
+#     end_date = DateField('End Date', validators=[Optional()])
+#     case_type = StringField('Case Type',
+#                                 validators=[DataRequired()])
+#     case_region = SelectField("Regional Office", validators=[DataRequired()], choices=Regions)
+#
+#     def __init__(self, *args, **kwargs):
+#         super(CaseForm, self).__init__(*args, **kwargs)
+#         self.user = None
+#
+#     def validate(self):
+#         initial_validation = super(CaseForm, self).validate()
+#         if not initial_validation:
+#             return False
+#         user = User.query.filter_by(username=self.username.data).first()
+#         if user:
+#             self.username.errors.append("Username already registered")
+#             return False
+#         user = User.query.filter_by(email=self.email.data).first()
+#         if user:
+#             self.email.errors.append("Email already registered")
+#             return False
+#         return True
 
