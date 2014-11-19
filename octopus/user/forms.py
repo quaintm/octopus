619ccabc2ffbca53892448e3a5a1320a8a0fcb44
martinow <martinow@sec.gov>
2014-11-19 11:08:42 -0500

    profile edit form and view comining onling
    Signed-off-by: martinow <martinow@sec.gov>

--- octopus/user/forms.py
+++ octopus/user/forms.py
@@ -1,8 +1,11 @@
 from flask_wtf import Form
 from wtforms import PasswordField, StringField
-from wtforms.validators import DataRequired, Email, EqualTo, Length
+from wtforms.validators import DataRequired, Email, EqualTo, Length, Optional
+from flask.ext.login import current_user, login_user
 
 from .models import User
+from octopus.utils import RequiredIf
+
 
 class RegisterForm(Form):
     username = StringField('Username',
@@ -31,3 +34,71 @@ class RegisterForm(Form):
             self.email.errors.append("Email already registered")
             return False
         return True
+
+class EditUserProfile(Form):
+    username = StringField('Username',
+                           validators=[Optional(), Length(min=3, max=25)])
+    email = StringField('Email',
+                        validators=[Optional(), Email(), Length(min=6, max=40)])
+    old_password_confirm = PasswordField('Old Password',
+                             validators=[RequiredIf('new_password'), Length(min=6, max=40)])
+    new_password = PasswordField('New Password',
+                             validators=[Length(min=6, max=40)])
+    new_confirm = PasswordField('Verify New Password',
+                            [RequiredIf('new_password'), EqualTo('new_password', message='Passwords must match')])
+    first_name = StringField('First Name',
+                             validators=[Optional()])
+    last_name = StringField('Last Name', validators=[Optional()])
+
+    def __init__(self, user, *args, **kwargs):
+        super(EditUserProfile, self).__init__(*args, **kwargs)
+        self.user = user
+        self.updated_attrs = dict()
+
+    def validate(self):
+        initial_validation = super(EditUserProfile, self).validate()
+        if not initial_validation:
+            return False
+        valid = True
+
+        # Reset Password Logic
+        if self.old_password_confirm.data:
+            if self.new_password.data:
+                if self.new_confirm.data:
+                    if not current_user.check_password(self.old_password_confirm.data):
+                        self.old_password_confirm.errors.append('Old password was incorrect')
+                        valid = False
+                    else:
+                        if not self.new_password.data == self.new_confirm.data:
+                            self.new_confirm.errors('Verify New Password field does not match New Password Field')
+                            valid = False
+                else:
+                    self.new_confirm.errors.append('Confirm Password Field was blank')
+                    valid = False
+
+        if self.username.data:
+            new_username = User.query.filter_by(username=self.username.data).first()
+            if new_username and not (current_user.username == self.username.data):
+                # Usernames should be unique
+                self.username.errors.append('This username is already taken')
+                valid = False
+
+        return valid
+
+def save_profile_edits(form):
+    if form.new_password.data:
+        current_user.set_password(form.new_password.data)
+    current_user.first_name = form.first_name.data
+    current_user.last_name = form.last_name.data
+
+    if not current_user.username == form.username.data:
+        #need to relogin the user as their username has changed
+        relogin = True
+    else:
+        relogin = False
+    current_user.username = form.last_name.data
+    current_user.email = form.last_name.data
+    current_user.save()
+
+
+
--- octopus/user/views.py
+++ octopus/user/views.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template
-from flask.ext.login import login_required
+from flask import Blueprint, render_template, request, redirect, flash, url_for
+from flask.ext.login import login_required, current_user
+
 from octopus.extensions import nav
+from octopus.user.forms import EditUserProfile, save_profile_edits
+from octopus.user.models import User
+from octopus.utils import flash_errors
+
 
 blueprint = Blueprint("user", __name__, url_prefix='/users',
                         static_folder="../static")
@@ -9,9 +14,7 @@ blueprint = Blueprint("user", __name__, url_prefix='/users',
 nav.Bar('user', [
     nav.Item('User', '', items=[
         nav.Item('Members', 'user.members'),
-        nav.Item('second drop', '', items=[
-            nav.Item('Members2', 'user.members')
-        ])
+        nav.Item('Edit My Profile', 'edit_profile')
     ])
 ])
 
@@ -19,3 +22,20 @@ nav.Bar('user', [
 @login_required
 def members():
     return render_template("users/members.html")
+
+@blueprint.route("/profile/<int:id>/edit", methods=["GET", "POST"])
+@blueprint.route("profile/edit", methods=["GET", "POST"])
+@login_required
+def edit_profile(id=current_user.get_id()):
+    user = User.query.filter_by(id=id).first_or_404()
+    form = EditUserProfile(request.form, user)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            save_profile_edits(form)
+            flash("User Profile Edits Saved")
+            redirect_url = request.args.get("next") or url_for("user.profile", id=id)
+            return redirect(redirect_url)
+        else:
+            flash_errors(form)
+    return render_template("users/edit_profile.html", user=user)
+
