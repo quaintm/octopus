5ca3b520d7970608d7e82d0ee3a24c45c0ce830e
buckie <wjmartino@gmail.com>
2014-11-20 01:28:56 -0500

    got data tables working
    Signed-off-by: buckie <wjmartino@gmail.com>

--- octopus/assets.py
+++ octopus/assets.py
@@ -3,6 +3,8 @@ from flask.ext.assets import Bundle, Environment
 
 css = Bundle(
     "libs/bootstrap/dist/css/bootstrap.css",
+    "libs/dataTables/dataTables.bootstrap.css",
+    "libs/font-awesome4/css/font-awesome.css",
     "css/style.css",
     filters="cssmin",
     output="public/css/common.css"
@@ -12,6 +14,8 @@ js = Bundle(
     "libs/jQuery/dist/jquery.js",
     "libs/bootstrap/dist/js/bootstrap.js",
     "js/plugins.js",
+    "libs/dataTables/jquery.dataTables.js",
+    "libs/dataTables/dataTables.bootstrap.js",
     filters='jsmin',
     output="public/js/common.js"
 )
